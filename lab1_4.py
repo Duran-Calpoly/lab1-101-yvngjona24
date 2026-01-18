d9f862eb8ecc6f4f7701228f99596a8816e6e8f1
yvngjona24 <jmart855@calpoly.edu>
2026-01-18 04:57:36 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,6 @@
-pass
+def calculate_average(x=10,y=20, z=30):#it calculates the average for whole numbers and  whole numbers with decimal points
+    return (x+y+z)/3
+def add_tax(x=100):# it adds 10% tax to the given amount
+    return x/10+x 
+def greet_user(name:str):#it greets the user by insterting their name into the string
+    return "Hello "+name
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,23 @@
-pass
+def check_multiple (number):
+    if number % 3 == 0 and number % 5 ==0:
+         return True
+    else:
+         return False
+
+def check_password(password):
+     secret_word = "Python123"
+     if password==secret_word:
+          return "access granted"
+     else:
+          return "access denied"
+
+
+def calculate_federal_tax(salary):
+    if salary <=11000:
+         return salary*0.10
+    elif salary <=44725:
+         return salary*0.12
+    elif salary<=95375:
+         return salary*0.22
+    else:
+         return salary*0.24
